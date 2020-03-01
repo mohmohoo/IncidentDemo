b993cc5608cb17792792964f9ad710623811f084
mohmohoo <mohmohoo85@gmail.com>
2020-03-01 16:51:10 +0000

    Add repository class

---
+++ IncidentDemo/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using IncidentDemo.Models;
+using System.Collections.Generic;
+
+namespace IncidentDemo.Repository
+{
+    public interface IRepository
+    {
+        IResult<Incident> Add(Incident incident);
+
+        IResult<Incident> Update(Incident incident);
+
+        IResult<Incident> Delete(Incident incident);
+
+        IResult<IEnumerable<Incident>> List();
+    }
+}
---
+++ IncidentDemo/Repository/IResult.cs
@@ -0,0 +1,13 @@
+﻿namespace IncidentDemo.Repository
+{
+    public interface IResult<TOutput>
+    {
+        TOutput Output { get; }
+
+        bool Success { get; }
+
+        string UserMessage { get; }
+
+        string SystemMessage { get; }
+    }
+}
